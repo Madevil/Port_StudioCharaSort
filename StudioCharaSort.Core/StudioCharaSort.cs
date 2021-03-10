e64846b35fd88a2d2cb29b0e85d4027deec20ff9
Madevil <meowmeowandy@gmail.com>
2021-03-10 17:56:23 +0800

    r4

--- StudioCharaSort.Core/StudioCharaSort.cs
+++ StudioCharaSort.Core/StudioCharaSort.cs
@@ -1,8 +1,13 @@
 using System;
 using System.Reflection;
+using System.Collections.Generic;
+using System.IO;
+
 using BepInEx;
 using BepInEx.Configuration;
+using BepInEx.Logging;
 using HarmonyLib;
+
 using Studio;
 
 namespace StudioCharaSort
@@ -19,16 +24,19 @@ namespace StudioCharaSort
 		public const string GUID = "kky.kk.studiocharasort";
 #endif
 		public const string PluginName = "Studio Character Sort";
-		internal const string Version = "1.0.2.0";
+		internal const string Version = "1.0.3.0";
 
 		public static ConfigEntry<sortTypes> ConfigSType;
 		public static ConfigEntry<sortOrders> ConfigSOrder;
+
+		internal static new ManualLogSource Logger;
 		public enum sortTypes { Name, Date }
 		public enum sortOrders { Descending, Ascending }
 		public static bool sortAscend;
 
 		private void Awake()
 		{
+			Logger = base.Logger;
 			ConfigSType = Config.Bind("Character cards default sort values. Changes take effect at next startup.", "Sort By", sortTypes.Name, "Set custom default sort type. Game default is Name");
 			ConfigSOrder = Config.Bind("Character cards default sort values. Changes take effect at next startup.", "Sort Order", sortOrders.Descending, "Set custom default sort order. Game default is Descending");
 			sortAscend = (ConfigSOrder.Value != sortOrders.Descending);
@@ -46,6 +54,46 @@ namespace StudioCharaSort
 			if (flag)
 				___charaFileSort.Sort((int) ConfigSType.Value, sortAscend);
 		}
+#if KK
+		[HarmonyPriority(Priority.Last)]
+		[HarmonyPrefix, HarmonyPatch(typeof(CharaList), "InitFemaleList")]
+		internal static bool CharaList_InitFemaleList_Prefix(CharaFileSort ___charaFileSort)
+		{
+			Logger.LogDebug($"[CharaList_InitFemaleList_Prefix]");
+			CharaList_InitList(___charaFileSort, 1);
+			return false;
+		}
+
+		[HarmonyPriority(Priority.Last)]
+		[HarmonyPrefix, HarmonyPatch(typeof(CharaList), "InitMaleList")]
+		internal static bool CharaList_InitMaleList_Prefix(CharaFileSort ___charaFileSort)
+		{
+			Logger.LogDebug($"[CharaList_InitMaleList_Prefix]");
+			CharaList_InitList(___charaFileSort, 0);
+			return false;
+		}
+
+		internal static void CharaList_InitList(CharaFileSort charaFileSort, byte sex = 0)
+		{
+			List<string> files = new List<string>();
+			string folder = UserData.Path + "chara/" + (sex == 0 ? "male" : "female");
+			Illusion.Utils.File.GetAllFiles(folder, "*.png", ref files);
+			charaFileSort.cfiList.Clear();
+			ChaFileControl chaFileControl = new ChaFileControl();
+			foreach (string item in files)
+			{
+				if (chaFileControl.LoadCharaFile(item, sex, noLoadPng: true))
+				{
+					charaFileSort.cfiList.Add(new CharaFileInfo(string.Empty, string.Empty)
+					{
+						file = item,
+						name = chaFileControl.parameter.fullname,
+						time = File.GetLastWriteTime(item)
+					});
+				}
+			}
+		}
+#endif
 	}
 
 	public class Patches
